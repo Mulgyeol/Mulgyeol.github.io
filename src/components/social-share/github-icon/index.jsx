f5253508a26c617f0178bfa59338cba92829448d
Mulgyeol <mulgyeoljung@gmail.com>
2021-04-15 21:23:47 +0900

    Update account info

--- src/components/social-share/github-icon/index.jsx
+++ src/components/social-share/github-icon/index.jsx
@@ -5,7 +5,7 @@ import './index.scss'
 export const GitHubIcon = () => {
   return (
     <a
-      href="https://github.com/JaeYeopHan/felog"
+      href="https://github.com/Mulgyeol/Mulgyeol.github.io"
       className="github"
       aria-label="GitHub"
       target="_blank"
